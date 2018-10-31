9fcf4a110953db4d6eb1730a91e2f52704021fb5
Rachel Chang <rjchang96@gmail.com>
2018-10-31 13:44:52 -0600

    I added more comments haha

--- app/src/main/java/nothelloworld/sociallive/MainActivity.java
+++ app/src/main/java/nothelloworld/sociallive/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //this is a comment
+    //more comments hi peeps!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
