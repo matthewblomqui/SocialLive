bdd1e176f7d801e9383f611b709b03f17a875788
jonmichaelossola <jm.ossola24@gmail.com>
2018-11-19 13:47:21 -0700

    Added previous changes back on.

--- app/src/main/java/nothelloworld/sociallive/MainActivity.java
+++ app/src/main/java/nothelloworld/sociallive/MainActivity.java
@@ -12,7 +12,7 @@ import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import java.util.List;
 
@@ -26,10 +26,15 @@ public class MainActivity extends AppCompatActivity {
     // have to do with animations regarding the different windows.
     public long animationDuration = 1000;
     private ConstraintLayout eventCategoryFinder = null;
+    private LinearLayout categoriesLayout = null;
 
     // This is the pop up window to create a new event
     Dialog createEventDialog;
 
+    /*********************************************************************************
+     * On create. Create all of the backbone of what we are doing by creating and
+     * initializing the variables.
+     * **********************************************************************************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
         // find the overlay
         eventCategoryFinder = findViewById(R.id.findEventWindow);
+        categoriesLayout = findViewById(R.id.categoriesLayout);
 
         // Get the size of the screen/window
         Display display = getWindowManager().getDefaultDisplay();
@@ -46,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
 
         // translate the new window off screen without animation before anyone can see it
         eventCategoryFinder.setX(size.x);
+        categoriesLayout.setX(size.x);
+
 
         // declare Dialog variable for pop up window
         createEventDialog = new Dialog(this);
@@ -54,6 +62,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /*********************************************************************************
+     * The user has just created a username and hit save. Now let's save their
+     * username and then launch the create event window.
+     * **********************************************************************************/
     public void createIdentification(View v) {
 
         EditText edit = (EditText)createEventDialog.findViewById(R.id.user);
@@ -65,18 +77,30 @@ public class MainActivity extends AppCompatActivity {
         // store username from text editor in the login page.
         //SharedPreferences.Editor theEditor = userPreferences.edit();
         // WARNING: ERROR IS HERE
+
         prefEditor.apply();
         //theEditor.putString("username", result);
 
         createEventDialog.dismiss();
+
+        // now return us to the other window.
+        createEventDialog.setContentView(R.layout.createpartypopup);
     }
 
+    /*********************************************************************************
+     * Launch the find event window, animate it onto the screen.
+     * then we are done! Pretty simple ay?
+     * **********************************************************************************/
     public void launchFindEventWindow(View v) {
 
         //move overlay on screen by animating the x value
         eventCategoryFinder.animate().x(0).setDuration(animationDuration);
     }
 
+    /*********************************************************************************
+     * Launch the create event window. But first, check if the user is signed in, with
+     * their username and password saved into Shared Preferences.
+     * **********************************************************************************/
     public void createEvent(View v) {
 
         // launch dialog activity
@@ -92,19 +116,48 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void returnToFeed(View v) {
+    /*********************************************************************************
+     * Simply dismiss the pop up to create an event.
+     *
+     * **********************************************************************************/
+    public void returnToFeedFromCreateEventPopUp(View v) {
 
         // Dismiss the pop up window
         createEventDialog.dismiss();
     }
 
+    /*********************************************************************************
+     * Launch the Board Game Suggestions page
+     *
+     * **********************************************************************************/
     public void toBoardGameSuggestions(View v) {
 
-        // move overlay
+        // first things first, get the current window off the screen.
         eventCategoryFinder.animate().x(eventCategoryFinder.getWidth()).setDuration(animationDuration);
+
+        // move overlay
+        categoriesLayout.animate().x(0).setDuration(animationDuration);
     }
 
+    /*********************************************************************************
+     * This will launch the video game suggestions page.
+     *
+     * **********************************************************************************/
     public void toVideoGameSuggestions(View v) {
         
+    }
+
+     /*********************************************************************************
+     * Return to the feed from the categories page, by pushing off the categories page
+     * as well as the event find page.
+     * **********************************************************************************/
+    public void returnToFeedFromCategoriesPage(View v) {
+
+        // Push the categories page off the screen
+        categoriesLayout.animate().x(categoriesLayout.getWidth()).setDuration(animationDuration);
+
+        // if the find events page is still on screen, push it off.
+        if (eventCategoryFinder.getX() == 0)
+            eventCategoryFinder.animate().x(eventCategoryFinder.getWidth()).setDuration(animationDuration);
     }
 }
--- app/src/main/java/nothelloworld/sociallive/UserPreferences.java
+++ app/src/main/java/nothelloworld/sociallive/UserPreferences.java
@@ -1,5 +1,6 @@
 package nothelloworld.sociallive;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserPreferences {
@@ -12,6 +13,16 @@ public class UserPreferences {
     }
 
     public List<String> getUserPreferences() { return userPreferences; }
+
+    public void addToPreferences(String addMe) {
+
+        if (userPreferences == null) {
+            userPreferences = new ArrayList<>();
+        }
+
+        userPreferences.add(addMe);
+
+    }
 
     public void setUserPreferences(List<String> userPreferences) { this.userPreferences = userPreferences; }
 }
