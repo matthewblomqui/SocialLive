de3cec2f876885c122394d1bd3ba666057580e1c
jonmichaelossola <jm.ossola24@gmail.com>
2018-11-19 13:06:22 -0700

    Added a user preferences class, modified a function inside of main activity

--- app/src/main/java/nothelloworld/sociallive/MainActivity.java
+++ app/src/main/java/nothelloworld/sociallive/MainActivity.java
@@ -139,7 +139,7 @@ public class MainActivity extends AppCompatActivity {
      *
      *******************************************************************/
     public void toVideoGameSuggestions(View v) {
-
+        
     }
 
     /**************************************************************
