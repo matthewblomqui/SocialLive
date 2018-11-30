c83b9b0829f64e961141fd420a58622396c87e27
taylormatheny <mat15018@byui.edu>
2018-11-30 13:22:49 -0700

    This is the code to add a party to the database. It is not yet perfect.

--- app/src/main/java/nothelloworld/sociallive/Party.java
+++ app/src/main/java/nothelloworld/sociallive/Party.java
@@ -6,76 +6,34 @@ import java.util.List;
 
 public class Party {
 
-    private User host;
-    private String title;
-    private Date date;
-    private Time startTime;
-    private Time endTime;
+    //private User host;
+    //private String title;
+    private String location;
     private String description;
-    private int numPeopleGoing;
+    private String startTime;
+    private String endTime;
+    private String date;
+    private String dateCreated;
+    private String partyId;
 
-
-    public Party(String title, Date date, Time startTime, Time endTime, int numPeopleGoing, String description) {
-        this.title = title;
-        this.date = date;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.numPeopleGoing = numPeopleGoing;
-        this.description = description;
-    }
-
-    public void setNumPeopleGoing(int numPeopleGoing) {
-        this.numPeopleGoing = numPeopleGoing;
-    }
-
-    public int getNumPeopleGoing()
+    public Party(String location, String description, String startTime,
+                 String endTime, String date, String dateCreated,
+                 String partyId)
     {
-        return numPeopleGoing;
-    }
-
-    public User getHost() {
-        return host;
-    }
-
-    public void setHost(User host) {
-        this.host = host;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public Time getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Time startTime) {
+        this.location = location;
+        this.description = description;
         this.startTime = startTime;
-    }
-
-    public Time getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(Time endTime) {
         this.endTime = endTime;
+        this.date = date;
+        this.dateCreated = dateCreated;
+        this.partyId = partyId;
     }
 
-    public String getAdditionalInfo() { return description; }
-
-    public void setAdditionalInfo(String additionalInfo) {
-        this.description = additionalInfo;
-    }
+    public String getLocation() { return location; }
+    public String getDescription() { return description; }
+    public String getStartTime() { return startTime; }
+    public String getEndTime() { return endTime; }
+    public String getDate() { return date; }
+    public String getDateCreated() { return dateCreated; }
+    public String getPartyId() { return partyId; }
 }
