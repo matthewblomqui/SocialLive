a380888a6a37a86b212bf43f3bc9804a6393941f
Matthew Blomquist <matthewblomquist95@gmail.com>
2018-12-04 20:38:57 -0700

    Added beginnings of activities and intents

---
+++ app/src/main/java/nothelloworld/sociallive/FindEvents.java
@@ -0,0 +1,13 @@
+package nothelloworld.sociallive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_events);
+    }
+}
